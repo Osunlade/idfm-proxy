d30705ccec537d056c1962fd41efc4f7f5df1db5
Osunlade <osunlade91@gmail.com>
2025-11-18 10:27:45 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const API_URL = "https://prim.iledefrance-mobilites.fr/marketplace/gtfs-rt/vehicle-position";
+const API_KEY = "TA_CLE_API_IDFM";  // mets ta clé ici
+
+app.get("/rer", async (req, res) => {
+    try {
+        const response = await axios.get(API_URL, {
+            headers: { apikey: API_KEY },
+            responseType: "arraybuffer"
+        });
+        res.send(response.data);
+    } catch (err) {
+        res.status(500).json({ error: err.toString() });
+    }
+});
+
+app.listen(3000, () => console.log("Proxy running on port 3000"));
